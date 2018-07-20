75e078245e34c6de15ec284b87d1cd724dfc4389
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2018-07-19 23:04:22 -0400

    added albums section, no relationships set

---
+++ scripts/albums/album-post.sh
@@ -0,0 +1,11 @@
+curl "http://localhost:4741/albums" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "album": {
+      "title": "'"${TITLE}"'",
+      "artist": "'"${ARTIST}"'",
+      "year": "'"${YEAR}"'"
+    }
+  }'
